00edae17002349760b615aa53da0ab69bea6c010
a1exb1 <alex_bechmann@hotmail.com>
2015-05-13 19:07:47 +0200

    dont need ## ? again

--- CompresJSON/Classes/Library/Action filters/WebApiApplyEncryptionAndCompression.cs
+++ CompresJSON/Classes/Library/Action filters/WebApiApplyEncryptionAndCompression.cs
@@ -51,8 +51,10 @@ namespace CompresJSON
             {
                 //assume encrypted + compressed for now
                 var d = httpBodyDictionary["data"];
+                var x = Encrypter.Decrypt(d);
+                var y = Compressor.Decompress(x);
 
-                string json = CompresJSONUtilities.DecryptAndDecompressAsNecessary(d).Replace("#", "\"");
+                string json = CompresJSONUtilities.DecryptAndDecompressAsNecessary(d) ;//.Replace("#", "\"");
                 var dict = new JavaScriptSerializer().Deserialize<Dictionary<string, string>>(json);
 
                 foreach (var key in dict.Keys)
